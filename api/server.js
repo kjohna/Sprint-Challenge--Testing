b4341046e42c75f5d8e249ed184a97c8b533f4c8
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-08 09:35:34 -0700

    initial server setup

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+server.get('/', async (req, res) => {
+  try {
+    res.status(200).json({ message: 'server listening!' });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+module.exports = server;
