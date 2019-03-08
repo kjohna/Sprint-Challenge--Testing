56eead7e9ba003b5b4ca7d1d720db40e50553a7f
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-08 09:41:43 -0700

    tests for basic server functionality

--- api/server.spec.js
+++ api/server.spec.js
@@ -2,11 +2,25 @@ const request = require('supertest');
 const server = require('./server.js');
 
 describe('server.js', () => {
+  it('should set testing environment', () => {
+    expect(process.env.DB_ENV).toBe('testing');
+  });
+
   describe('GET /', () => {
     // check that server starts and is listening
     it('should return status 200', async () => {
       const res = await request(server).get('/');
       expect(res.status).toBe(200);
-    })
+    });
+    // test response data type is JSON
+    it('should return JSON', async () => {
+      const res = await request(server).get('/');
+      expect(res.type).toBe('application/json');
+    });
+    // test response data shape
+    it('should return { message: "server listening!"', async () => {
+      const res = await request(server).get('/');
+      expect(res.body).toEqual({ message: 'server listening!' });
+    });
   });
 });
